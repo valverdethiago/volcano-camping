1a07538e09b1cf8f72f5cffba3b662ddbf51a29b
Thiago Valverde de Souza <valverde.thiago@gmail.com>
2019-02-04 18:11:44 -0200

    Add implementation to query the reservations within a period

--- src/main/java/com/upgrade/volcanocamping/repositories/ReservationRepository.java
+++ src/main/java/com/upgrade/volcanocamping/repositories/ReservationRepository.java
@@ -5,12 +5,23 @@ package com.upgrade.volcanocamping.repositories;
 
 import com.upgrade.volcanocamping.model.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 /**
- * Member repository for basic operations on Reservation entity.
+ * Repository for basic operations on Reservation entity.
  * @author valverde.thiago
  */
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+    @Query(" select e from Reservation e " +
+            " where e.initialDate between :initialDate and :endDate" +
+            "    or e.departureDate between :initialDate and :endDate " )
+    List<Reservation> findReservationsBetweenDates(@Param("initialDate") LocalDateTime initialDate,
+                                                   @Param("endDate") LocalDateTime endDate);
 }
--- src/test/java/com/upgrade/volcanocamping/repositories/ReservationRepositoryTest.java
+++ src/test/java/com/upgrade/volcanocamping/repositories/ReservationRepositoryTest.java
@@ -12,6 +12,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.time.Month;
 import java.util.List;
 import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.*;
@@ -24,24 +25,55 @@ public class ReservationRepositoryTest {
 	
 	@Autowired
 	private ReservationRepository reservationRepository;
-	Faker faker = new Faker();
+	private Faker faker = new Faker();
 	
 	@Test
 	public void shouldInsertSimpleReservation() {
 		// Arrange
+		Reservation reservation = this.createAndSaveReservation("fake-member@gmail.com",
+				LocalDateTime.now(), LocalDateTime.now());
+		// Act
+		List<Reservation> reservationList = Lists.newArrayList(this.reservationRepository.findAll());
+		// Assert
+		assertNotNull(reservation.getId());
+		assertThat(reservationList, containsInAnyOrder(reservation));
+	}
+
+	@Test
+	public void shouldReturnSingleReservationInGivenPeriod() {
+		// Arrange
+		Reservation reservation = this.createAndSaveReservation("fake-member@gmail.com",
+				LocalDateTime.of(2019, Month.JANUARY, 1, 12, 00, 00),
+				LocalDateTime.of(2019, Month.JANUARY, 3, 12, 00, 00));
+		Reservation reservation2 = this.createAndSaveReservation("fake-member2@gmail.com",
+				LocalDateTime.of(2019, Month.FEBRUARY, 1, 12, 00, 00),
+				LocalDateTime.of(2019, Month.FEBRUARY, 3, 12, 00, 00));
+		// Act
+		List<Reservation> reservationList = this.reservationRepository.findReservationsBetweenDates(
+				LocalDateTime.of(2019, Month.JANUARY, 1, 0, 0, 0),
+				LocalDateTime.of(2019, Month.JANUARY, 31, 23, 59, 59)
+		);
+
+		// Assert
+		assertThat(reservationList, not(empty()));
+		assertThat(reservationList, hasSize(1) );
+		assertThat(reservationList, hasItem(reservation));
+		assertThat(reservationList, not(hasItem(reservation2)));
+
+	}
+
+
+	private Reservation createAndSaveReservation(String memberEmail,
+												LocalDateTime initialDate,
+												LocalDateTime departureDate) {
 		User user = new User();
-		user.setEmail("fake-member@gmail.com");
+		user.setEmail(memberEmail);
 		user.setFullName(faker.name().fullName());
 		Reservation reservation = new Reservation();
 		reservation.setUser(user);
-		reservation.setInitialDate(LocalDateTime.now());
-		reservation.setDepartureDate(LocalDateTime.now());
-		// Act
-		Reservation dbReservation = this.reservationRepository.save(reservation);
-		List<Reservation> reservationList = Lists.newArrayList(this.reservationRepository.findAll());
-		// Assert
-		assertNotNull(dbReservation.getId());
-		assertThat(reservationList, containsInAnyOrder(reservation));
+		reservation.setInitialDate(initialDate);
+		reservation.setDepartureDate(departureDate);
+		return this.reservationRepository.save(reservation);
 	}
 
 }
