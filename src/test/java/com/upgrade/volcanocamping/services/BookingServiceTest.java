1e04b71adfdd035680cc245ad337cf7587719c00
Thiago Valverde de Souza <valverde.thiago@gmail.com>
2019-02-06 19:15:45 -0200

    Fix unit test to mock repository instance

--- src/test/java/com/upgrade/volcanocamping/services/BookingServiceTest.java
+++ src/test/java/com/upgrade/volcanocamping/services/BookingServiceTest.java
@@ -1,39 +1,52 @@
 package com.upgrade.volcanocamping.services;
 
 import com.github.javafaker.Faker;
+import com.google.common.collect.ImmutableList;
 import com.upgrade.volcanocamping.model.Booking;
 import com.upgrade.volcanocamping.model.User;
 import com.upgrade.volcanocamping.repositories.BookingRepository;
 import com.upgrade.volcanocamping.service.BookingService;
+import com.upgrade.volcanocamping.service.BookingServiceImpl;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Set;
 
 import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class BookingServiceTest {
 
-    @Autowired
     private BookingService bookingService;
-    @Autowired
+    @Mock
     private BookingRepository bookingRepository;
 
+    @Before
+    public void setup() {
+        this.bookingService = new BookingServiceImpl(this.bookingRepository);
+    }
+
+
     @Test
-    public void givenOneReservationsThenReturnEmptyAvailableDates() {
+    public void givenOneBookingThenReturnEmptyAvailableDates() {
         // Arrange
         Booking booking = this.createFakeBooking("fake-user@mail.com",
                 LocalDateTime.now().minusDays(12).toLocalDate(),
                 LocalDateTime.now().minusDays(10).toLocalDate());
+        when(bookingRepository.findReservationsBetweenDates(any(), any()))
+                .thenReturn(ImmutableList.of(booking));
         // Act
         Set<LocalDate> availableDates = this.bookingService.findAvailableDates(
                 booking.getInitialDate(),
@@ -45,9 +58,11 @@ public class BookingServiceTest {
     @Test
     public void givenOneBookingOf2DaysThenReturn28AvailableDates() {
         // Arrange
-        this.createFakeBooking("fake-user@mail.com",
+        Booking booking = this.createFakeBooking("fake-user@mail.com",
                 LocalDate.now().plusDays(10),
                 LocalDate.now().plusDays(12));
+        when(bookingRepository.findReservationsBetweenDates(any(), any()))
+                .thenReturn(ImmutableList.of(booking));
         // this reservations has 2 days and the search period would be 30 days
         int expectedDatesToReturn = 30-2;
         // Act
@@ -60,7 +75,8 @@ public class BookingServiceTest {
     @Test
     public void givenEmptyBookingListThenReturn30AvailableDates() {
         // Arrange
-        this.bookingRepository.deleteAll();
+        when(bookingRepository.findReservationsBetweenDates(any(), any()))
+                .thenReturn(new ArrayList<>());
         // Act
         // at this case the service is expected to use the current date as initial and 30 days after as endDate
         Set<LocalDate> availableDates = this.bookingService.findAvailableDates(null, null);
@@ -79,6 +95,6 @@ public class BookingServiceTest {
         booking.setUser(user);
         booking.setInitialDate(initialDate);
         booking.setDepartureDate(departureDate);
-        return this.bookingRepository.save(booking);
+        return booking;
     }
 }
